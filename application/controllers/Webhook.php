5ef1df54a68d9afcf49b062d3049d17e250e9dca
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-11 13:37:15 +0800

    send Latitude

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -134,7 +134,7 @@ class Webhook extends CI_Controller {
       } else {
         $message = 'Silakan kirim pesan "MULAI" untuk memulai kuis(textMessage)';
         $textMessageBuilder = new TextMessageBuilder($message);
-        if($latitude!=''){
+        if($userLocation!=''){
           $this->bot->replyMessage($event['replyToken'], "Lokasi Latitude".$latitude);
         }
         else {
