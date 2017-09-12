93dfa9effc3dab89bc31d1ce29c3acec73eade2a
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-12 16:30:58 +0800

    Get User Location from user share location

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -68,8 +68,7 @@ class Webhook extends CI_Controller {
             }
             if($event['message']['type'] == 'location')
             {
-              $alamat = $event['message']['address'];
-              $location = new LocationMessageBuilder('tes', $alamat, '-33.8670522', '151.1957362');
+              $location = new LocationMessageBuilder('tes', 'lagi', '-33.8670522', '151.1957362');
               $this->bot->replyMessage($event['replyToken'], $location);
             }
           } else {
