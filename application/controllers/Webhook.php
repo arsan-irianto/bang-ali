db2eb2ebefcb6c50f4e6705b3c1e3238f8fe24c8
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-13 14:51:24 +0800

    Send Button Template Message Masjid Image

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -171,7 +171,6 @@ class Webhook extends CI_Controller {
       $latMasjid = $result['results'][1]['geometry']['location']['lat'];
       $lngMasjid = $result['results'][1]['geometry']['location']['lng'];
 
-      $result['results'][1]['photos'][0]['photo_reference'] =
       $urlPhotoMasjidTerdekat="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400";
       $urlPhotoMasjidTerdekat.="&photoreference=".$result['results'][2]['photos'][]['photo_reference'];
       $urlPhotoMasjidTerdekat.="&key=AIzaSyDk0ZDDDMCFiVZUxwLsNlUPJwSiTxQzub4";
