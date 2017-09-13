ac85d6e8c6f4d2fc02a8a6ab6bba2e2db0cd52e7
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-13 14:37:08 +0800

    Send Button Template Message Masjid Image

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -177,11 +177,11 @@ class Webhook extends CI_Controller {
 
       //$location = new LocationMessageBuilder($namaMasjid, $alamatMasjid, $latMasjid, $lngMasjid);
       //$this->bot->replyMessage($event['replyToken'], $location);
-      $dummyImage = "https://lh3.googleusercontent.com/p/AF1QipMlZ-K8MWi-V2xHRD0035qtOwC8Jz7kcrpPGhjt=s1600-w400";
+      $dummyImage = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAAWtny1d7xbUw-CgOgeHGUZopUbSEPkpxD0aDyL5lIofUG_RULEtjbzX37IGNBc3O4En5-bO6Yr0kJRoIMTETFd_ty6lI6kLm2qKZJ5LhWawLqf0BlmkHiqf-8Xm3FNeT3EhB8pkJ04sPECNU6yJU3QBEoGhTXvdn51TawxFHoRWkQOcFWZqg8qg&key=AIzaSyDk0ZDDDMCFiVZUxwLsNlUPJwSiTxQzub4";
       //prepare options button
       $options[0] = new MessageTemplateActionBuilder('Detail Lokasi', 'detail lokasi');
       // prepare button template
-      $buttonTemplate = new ButtonTemplateBuilder($namaMasjid, $alamatMasjid, $urlPhotoMasjidTerdekat, $options);
+      $buttonTemplate = new ButtonTemplateBuilder($namaMasjid, $alamatMasjid, $dummyImage, $options);
 
       // build message
       $messageBuilder = new TemplateMessageBuilder("Gunakan mobile app untuk melihat soal", $buttonTemplate);
