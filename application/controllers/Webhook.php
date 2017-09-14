c2488fc4a19144b149893a8e069245c833a98b86
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-14 17:10:04 +0800

    Test Dummy One Click One Ayat

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -219,13 +219,13 @@ class Webhook extends CI_Controller {
         $textMessageBuilder = new TextMessageBuilder($message);
         $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
       }
-      elseif(strtolower($userMessage)=='one click one ayat'){
+      else{
         $dummyImage = "https://cdn.alquran.cloud/media/image/2/255";
         $dummyTranslation = "Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafa'at di sisi Allah tanpa izin-Nya? Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar.";
         //prepare options button
         $options[0] = new MessageTemplateActionBuilder('tes', 'tes');
         // prepare button template
-        $buttonTemplate = new ButtonTemplateBuilder('QS : Al-Baqarah[2]:255 ', $dummyTranslation, $dummyImage, $options);
+        $buttonTemplate = new ButtonTemplateBuilder('QS : Al-Baqarah ayat 2 255 ', $dummyTranslation, $dummyImage, $options);
 
         // build message
         $messageBuilder = new TemplateMessageBuilder("Gunakan mobile app untuk melihat soal", $buttonTemplate);
@@ -233,11 +233,11 @@ class Webhook extends CI_Controller {
         // send message
         $this->bot->replyMessage($event['replyToken'], $messageBuilder);
       }
-      else {
+/*      else {
         $message = 'Under Development...';
         $textMessageBuilder = new TextMessageBuilder($message);
         $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
-      }
+      }*/
   }
 
   private function stickerMessage($event)
