973c914dd7c7710d6af61852876e64b3abb62c5a
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-14 23:45:33 +0800

    Reply with dummy ayat and translation

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -259,7 +259,7 @@ class Webhook extends CI_Controller {
     $returnedTranslationAyat = $this->get_data($translationAyat);
     $resultTranslation = json_decode($returnedTranslationAyat,true);
 
-    $message = $resultAyat['data']['text'];
+    $message = $resultAyat['data']['text']."\n\n".$resultTranslation['data']['text'];
     $textMessageBuilder = new TextMessageBuilder($message);
     $this->bot->replyMessage($replyToken, $textMessageBuilder);
 
