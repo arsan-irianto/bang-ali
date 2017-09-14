a0efad2cb34a2bc6ccc90e00cefd92bb50543fce
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-14 16:50:36 +0800

    Send Dummy One Click One Ayat

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -223,6 +223,7 @@ class Webhook extends CI_Controller {
         $message = REPLY_MASJID_TERDEKAT;
         break;
       case 'one click one ayat':
+        $this->oneClickOneAyat($event['replyToken'], 'one click one ayat');
         break;
       case 'waktu shalat':
         break;
@@ -250,6 +251,23 @@ class Webhook extends CI_Controller {
     $multiMessageBuilder->add($textMessageBuilder);
     // send message
     $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
+  }
+
+  private function oneClickOneAyat($replyToken, $message){
+    if(strtolower($message)=='one click one ayat'){
+      $dummyImage = "https://cdn.alquran.cloud/media/image/2/255";
+      $dummyTranslation = "Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafa'at di sisi Allah tanpa izin-Nya? Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar.";
+      //prepare options button
+      $options[0] = new MessageTemplateActionBuilder('tes', 'tes');
+      // prepare button template
+      $buttonTemplate = new ButtonTemplateBuilder('QS : Al-Baqarah[2]:255 ', $dummyTranslation, $dummyImage, $options);
+
+      // build message
+      $messageBuilder = new TemplateMessageBuilder("Gunakan mobile app untuk melihat soal", $buttonTemplate);
+
+      // send message
+      $this->bot->replyMessage($replyToken, $messageBuilder);
+    }
   }
 
 }
