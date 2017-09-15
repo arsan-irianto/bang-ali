479aef7274c370d6010d4de694a2e3cd5b0b00f5
Arsan Irianto <arsan.irianto@gmail.com>
2017-09-15 16:21:35 +0800

    Check if user click masjid terdekat before share location

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -25,7 +25,6 @@ class Webhook extends CI_Controller {
   private $events;
   private $signature;
   private $user;
-  private $isUserClickMasjidTerdekat = 0;
 
   function __construct()
   {
@@ -145,56 +144,51 @@ class Webhook extends CI_Controller {
       $userLocation = $event['message']['type'];
       if($userLocation == 'location')
       {
-        if ( $this->isUserClickMasjidTerdekat == 1)
+        $locationFromUserShared = $event['message']['latitude'] . "," . $event['message']['longitude'];
+
+        $urlMasjidTerdekat ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+        $urlMasjidTerdekat .="location=". $event['message']['latitude'] . "," . $event['message']['longitude'];
+        $urlMasjidTerdekat .="&radius=500&type=mosque&keyword=masjid";
+        $urlMasjidTerdekat .="&key=".$_ENV['GMAPS_API_KEY'];
+
+        // get url maps to parse json
+        $returned_content = $this->get_data($urlMasjidTerdekat);
+        // Decode google maps json
+        $result = json_decode($returned_content,true);
+
+        $columnTemplateBuilders = array();
+        if(is_array($result['results']))
         {
-          $locationFromUserShared = $event['message']['latitude'] . "," . $event['message']['longitude'];
+          $i=0;
+          foreach($result['results'] as $resultItem) if ($i < 5) {
 
-          $urlMasjidTerdekat ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
-          $urlMasjidTerdekat .="location=". $event['message']['latitude'] . "," . $event['message']['longitude'];
-          $urlMasjidTerdekat .="&radius=500&type=mosque&keyword=masjid";
-          $urlMasjidTerdekat .="&key=".$_ENV['GMAPS_API_KEY'];
+            // Array Data Masjid
+            $namaMasjid[]= $resultItem['name'];
+            $alamatMasjid[] = $resultItem['vicinity'];
+            $latMasjid[] = $resultItem['geometry']['location']['lat'];
+            $lngMasjid[] = $resultItem['geometry']['location']['lng'];
 
-          // get url maps to parse json
-          $returned_content = $this->get_data($urlMasjidTerdekat);
-          // Decode google maps json
-          $result = json_decode($returned_content,true);
+            // Create link direction url
+            $urlDirection[] = "https://www.google.co.id/maps/dir/".$locationFromUserShared."/".$resultItem['geometry']['location']['lat'].",".$resultItem['geometry']['location']['lng']."/@".$locationFromUserShared.",17z";
 
-          $columnTemplateBuilders = array();
-          if(is_array($result['results']))
-          {
-            $i=0;
-            foreach($result['results'] as $resultItem) if ($i < 5) {
+            // Array Photo Masjid
+            $urlPhotoMasjidTerdekat[]="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=".$resultItem['photos'][0]['photo_reference']."&key=".$_ENV['GMAPS_API_KEY'];
 
-              // Array Data Masjid
-              $namaMasjid[]= $resultItem['name'];
-              $alamatMasjid[] = $resultItem['vicinity'];
-              $latMasjid[] = $resultItem['geometry']['location']['lat'];
-              $lngMasjid[] = $resultItem['geometry']['location']['lng'];
-
-              // Create link direction url
-              $urlDirection[] = "https://www.google.co.id/maps/dir/".$locationFromUserShared."/".$resultItem['geometry']['location']['lat'].",".$resultItem['geometry']['location']['lng']."/@".$locationFromUserShared.",17z";
-
-              // Array Photo Masjid
-              $urlPhotoMasjidTerdekat[]="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=".$resultItem['photos'][0]['photo_reference']."&key=".$_ENV['GMAPS_API_KEY'];
-
-              // Array Column Carousel for carousel Template Builder
-              $columnTemplateBuilder = new CarouselColumnTemplateBuilder(
-                $namaMasjid[$i],
-                $alamatMasjid[$i],
-                $urlPhotoMasjidTerdekat[$i], [
-                new UriTemplateActionBuilder('Detail Rute', $urlDirection[$i]),
-              ]);
-              array_push($columnTemplateBuilders, $columnTemplateBuilder);
-              $i++;
-            }
+            // Array Column Carousel for carousel Template Builder
+            $columnTemplateBuilder = new CarouselColumnTemplateBuilder(
+              $namaMasjid[$i],
+              $alamatMasjid[$i],
+              $urlPhotoMasjidTerdekat[$i], [
+              new UriTemplateActionBuilder('Detail Rute', $urlDirection[$i]),
+            ]);
+            array_push($columnTemplateBuilders, $columnTemplateBuilder);
+            $i++;
           }
-          // Carousel Template builder and send reply template message
-          $carouselTemplateBuilder = new CarouselTemplateBuilder($columnTemplateBuilders);
-          $templateMessage = new TemplateMessageBuilder('Gunakan mobile app untuk melihat pesan', $carouselTemplateBuilder);
-          $this->bot->replyMessage($event['replyToken'], $templateMessage);
-
-          $this->isUserClickMasjidTerdekat = 0;
         }
+        // Carousel Template builder and send reply template message
+        $carouselTemplateBuilder = new CarouselTemplateBuilder($columnTemplateBuilders);
+        $templateMessage = new TemplateMessageBuilder('Gunakan mobile app untuk melihat pesan', $carouselTemplateBuilder);
+        $this->bot->replyMessage($event['replyToken'], $templateMessage);
       }
   }
 
@@ -207,7 +201,6 @@ class Webhook extends CI_Controller {
         $message = 'Silahkan share lokasi kamu ya dengan fitur share location (tombol +, dan pilih location dan klik share location)';
         $textMessageBuilder = new TextMessageBuilder($message);
         $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
-        $this->isUserClickMasjidTerdekat = 1;
         break;
       case 'one click one ayat':
         $this->oneClickOneAyat($event['replyToken']);
